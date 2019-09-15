a8c5f004a4b6149bc8ea0c8a31f39126ea6e6487
Diego lanes <diegosilvalanes@hotmail.com>
2019-09-15 14:35:59 -0300

    Migration novamente

---
+++ Medix.Repositorio/Migrations/20190915171025_MigrationMedix.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Medix.Repositorio.Migrations
+{
+    public partial class MigrationMedix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
